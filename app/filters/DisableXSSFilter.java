f111692fc6bfc62b5c32be0c058c14e4022c3793
Andy Marke <andy.marke@gmail.com>
2017-12-13 11:06:13 +0000

    [AM] 122 Turn off XSS IE filter that prevents Base64 encode pararmeters fro being rendered correctly in the form

--- app/filters/DisableXSSFilter.java
+++ app/filters/DisableXSSFilter.java
@@ -1,12 +1,9 @@
 package filters;
 
-import akka.util.ByteString;
-import javax.inject.Inject;
-import play.libs.streams.Accumulator;
 import play.mvc.EssentialAction;
 import play.mvc.EssentialFilter;
-import play.mvc.Result;
 
+import javax.inject.Inject;
 import java.util.concurrent.Executor;
 
 public class DisableXSSFilter extends EssentialFilter {
@@ -20,9 +17,8 @@ public class DisableXSSFilter extends EssentialFilter {
 
     @Override
     public EssentialAction apply(EssentialAction next) {
-        return EssentialAction.of(request -> {
-            Accumulator<ByteString, Result> accumulator = next.apply(request);
-            return accumulator.map(result -> result.withHeader("X-XSS-Protection", "0"), executor);
-        });
+        return EssentialAction.of(
+                request -> next.apply(request).
+                map(result -> result.withHeader("X-XSS-Protection", "0"), executor));
     }
 }
