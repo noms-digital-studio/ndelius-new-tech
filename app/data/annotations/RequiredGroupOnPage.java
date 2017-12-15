b2b8e6c3a13aa5e715073442c3ac78c2ade67d2b
Andy Marke <andy.marke@gmail.com>
2017-12-15 16:05:25 +0000

    [AM] 81 Offender Assessments issues changes with associated details for each issue

---
+++ app/data/annotations/RequiredGroupOnPage.java
@@ -0,0 +1,17 @@
+package data.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RequiredGroupOnPage {
+
+    int value();
+
+    String group() default "defaultGroup";
+
+    boolean leader() default false;
+}
