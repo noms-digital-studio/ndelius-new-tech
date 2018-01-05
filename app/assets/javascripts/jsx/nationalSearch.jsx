e0a16a796274ebc09195f6b1b00cbacec47be657
Andy Marke <andy.marke@gmail.com>
2018-01-05 10:40:15 +0000

    [AM] Use babel plugin for JSX/2015 compilation

--- app/assets/javascripts/jsx/nationalSearch.jsx
+++ app/assets/javascripts/jsx/nationalSearch.jsx
@@ -1,3 +1,19 @@
+let data = {
+    results: [],
+    searchTerm: ""
+};
+
+const performSearch = _.debounce((searchTerm) => {
+
+    $.getJSON('spellcheck/' + searchTerm, data => {
+        data = {results: data, searchTerm: searchTerm};
+
+        ReactDOM.render(
+            <OffenderSearchPage search={data.searchTerm} results={data.results} />,
+            document.getElementById('content')
+        );
+    });
+}, 500);
 
 class ResultsRow extends React.Component {
 
@@ -30,41 +46,50 @@ class ResultsGrid extends React.Component {
 
 class OffenderSearch extends React.Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            name: "",
-            results: []
-        };
-    }
-
-    componentWillMount() {
-
-        this.performSearch = _.debounce(() => {
-
-            $.getJSON('spellcheck/' + this.state.name, data => {
-                this.setState({
-                    results: data
-                });
-            });
-        }, this.props.delay);
-    }
-
     render() {
-
-        let searchChange = ev => this.setState({ name: ev.target.value }, this.performSearch);
+        let { searchTerm } = this.props;
 
         return (
             <div>
-                <input value={this.state.name} onChange={searchChange} placeholder="Enter text here" />
-                <ResultsGrid {...this.state} />
+                <input className="form-control padded" value={searchTerm} onChange={(event) => performSearch(event.target.value)} placeholder="Find names, addresses, date of birth, CRN and more..." />
             </div>
         );
     }
 }
 
+const Banner = () => (
+    <div className="phase-banner mobile-pad no-cell"><p><strong className="phase-tag">PROTOTYPE</strong><span>This is an experimental service – your <a href="feedback">feedback</a> will help us to improve it.</span></p></div>
+);
+
+
+class OffenderSearchPage extends React.Component {
+    render() {
+        let { results, searchTerm } = this.props;
+
+        return (
+            <div>
+                <noscript>You need to enable JavaScript to run this app.</noscript>
+                <div id="root">
+                    <main id="content">
+                        <Banner/>
+                        <div>
+                            <div className="govuk-box-highlight blue">
+                                <h1 className="heading-large no-margin-top margin-bottom medium">Search for an offender</h1>
+                                <form className="padding-left-right"><OffenderSearch searchTerm={searchTerm}/></form>
+                                <p className="bold margin-top medium no-margin-bottom">Can't find who you are looking for? <a className="clickable white">Add a new offender</a></p></div>
+                        </div>
+                        <div className="padded mobile-pad">
+                            <ResultsGrid results={results} />
+                        </div>
+                    </main>
+                </div>
+            </div>);
+    }
+
+}
+
+
 ReactDOM.render(
-    <OffenderSearch delay={500} />,
+    <OffenderSearchPage search={data.searchTerm} results={data.results} />,
     document.getElementById('content')
 );
