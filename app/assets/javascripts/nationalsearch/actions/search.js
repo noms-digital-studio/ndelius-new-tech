a6c72c9587afecde48fbe99bf9eb3ee0fffa111e
Andy Marke <andy.marke@gmail.com>
2018-01-10 14:55:11 +0000

    [AM] Results page

--- app/assets/javascripts/nationalsearch/actions/search.js
+++ app/assets/javascripts/nationalsearch/actions/search.js
@@ -1,28 +1,41 @@
+export const REQUEST_SEARCH = 'REQUEST_SEARCH'
+export const SEARCH_RESULTS = 'SEARCH_RESULTS'
+export const CLEAR_RESULTS = 'CLEAR_RESULTS'
+
 function requestSearch(searchTerm){
     return {
-        type: 'REQUEST_SEARCH',
+        type: REQUEST_SEARCH,
         searchTerm
     }
 }
 
-function searchResults(results){
+function searchResults(searchTerm, results){
     return {
-        type: 'SEARCH_RESULTS',
+        type: SEARCH_RESULTS,
+        searchTerm,
         results
+    }
+}
+
+function clearResults(){
+    return {
+        type: CLEAR_RESULTS
     }
 }
 
 const performSearch = _.debounce((dispatch, searchTerm) => {
     $.getJSON('searchOffender/' + searchTerm, data => {
-        dispatch(searchResults(data))
+        dispatch(searchResults(searchTerm, data))
     });
 }, 500);
 
 
 
 export function search(dispatch, searchTerm) {
-
-    dispatch(requestSearch(searchTerm));
-
-    performSearch(dispatch, searchTerm);
+    if (searchTerm === '') {
+        dispatch(clearResults())
+    } else {
+        dispatch(requestSearch(searchTerm));
+        performSearch(dispatch, searchTerm);
+    }
 }
