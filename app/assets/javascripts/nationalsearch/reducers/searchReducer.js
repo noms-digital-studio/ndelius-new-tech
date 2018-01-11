b5200d291756de89f3988997a331ce9009b62ee7
Andy Marke <andy.marke@gmail.com>
2018-01-11 12:36:38 +0000

    [AM] Results page

--- app/assets/javascripts/nationalsearch/reducers/searchReducer.js
+++ app/assets/javascripts/nationalsearch/reducers/searchReducer.js
@@ -10,7 +10,7 @@ const searchResults = (state = {searchTerm: '', results: []}, action) => {
             return {searchTerm: action.searchTerm, results: state.results};
         case SEARCH_RESULTS:
             if (state.searchTerm === action.searchTerm) {
-                return {searchTerm: state.searchTerm, results: mapResults(action.results, state.searchTerm)};
+                return {searchTerm: state.searchTerm, results: mapResults(action.results.offenders, state.searchTerm)};
             }
             return state
         case CLEAR_RESULTS:
@@ -25,30 +25,30 @@ export default searchResults
 function mapResults(results = [], searchTerm) {
     return results.map(offenderDetails => {
         return {
-            offenderId: offenderDetails['OFFENDER_ID'],
-            firstName: offenderDetails['FIRST_NAME'],
-            surname: offenderDetails['SURNAME'],
-            crn: offenderDetails['CRN'],
-            dateOfBirth: '19/07/1965',
-            risk: offenderDetails['CURRENT_HIGHEST_RISK_COLOUR'],
-            currentOffender: offenderDetails['CURRENT_DISPOSAL'] === 1,
-            gender: offenderDetails['GENDER_ID'] === 545 ? 'Male' : 'Female',
-            age: 2017 - offenderDetails['DATE_OF_BIRTH_DATE'].substring(0, 4),
-            aliases: offenderDetails['ALIASES'].map((alias) => {
+            offenderId: offenderDetails.offenderId,
+            firstName: offenderDetails.firstName,
+            surname: offenderDetails.surname,
+            crn: offenderDetails.crn,
+            dateOfBirth: offenderDetails.dateOfBirth,
+            risk: offenderDetails.risk,
+            currentOffender: offenderDetails.currentOffender,
+            gender: offenderDetails.gender,
+            age: offenderDetails.age,
+            aliases: offenderDetails.aliases.map((alias) => {
                 return {
-                    firstName: alias['FIRST_NAME'],
-                    surname: alias['SURNAME']
+                    firstName: alias.firstName,
+                    surname: alias.surname
                 }
             }).filter(item => anyMatch(item, searchTerm)),
-            previousSurname: whenMatched(offenderDetails['PREVIOUS_SURNAME'], searchTerm),
-            addresses: offenderDetails['ADDRESSES'].map((address) => {
+            previousSurname: whenMatched(offenderDetails.previousSurname, searchTerm),
+            addresses: offenderDetails.addresses.map((address) => {
                 return {
-                    addressNumber: address['ADDRESS_NUMBER'].toString(),
-                    buildingName: address['BUILDING_NAME'],
-                    streetName: address['STREET_NAME'],
-                    town: address['TOWN_CITY'],
-                    county: address['COUNTY'],
-                    postcode: address['POSTCODE']
+                    addressNumber: address.addressNumber,
+                    buildingName: address.buildingName,
+                    streetName: address.streetName,
+                    town: address.town,
+                    county: address.county,
+                    postcode: address.postcode
                 }
             }).filter(item => anyMatch(item, searchTerm))
         }
