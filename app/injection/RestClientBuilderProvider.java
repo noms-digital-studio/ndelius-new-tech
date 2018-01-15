5cbab1b72640e7ad3ad9313199eda0990012ac43
Andy Marke <andy.marke@gmail.com>
2018-01-15 11:06:03 +0000

    [AM] Use https by default

--- app/injection/RestClientBuilderProvider.java
+++ app/injection/RestClientBuilderProvider.java
@@ -11,16 +11,18 @@ import javax.inject.Provider;
 public class RestClientBuilderProvider implements Provider<RestClientBuilder> {
 
     private final String elasticSearchHost;
+    private final String elasticSearchScheme;
     private final int elasticSearchPort;
 
     @Inject
     public RestClientBuilderProvider(Config configuration) {
         elasticSearchHost = configuration.getString("elastic.search.host");
         elasticSearchPort = configuration.getInt("elastic.search.port");
+        elasticSearchScheme = configuration.getString("elastic.search.scheme");
     }
 
     @Override
     public RestClientBuilder get() {
-        return RestClient.builder(new HttpHost(elasticSearchHost, elasticSearchPort));
+        return RestClient.builder(new HttpHost(elasticSearchHost, elasticSearchPort, elasticSearchScheme));
     }
 }
