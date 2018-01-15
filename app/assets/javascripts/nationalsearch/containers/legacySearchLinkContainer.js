13e24f141fb2a570d65fde7aa200989fa9dcd5fe
Andy Marke <andy.marke@gmail.com>
2018-01-15 15:25:03 +0000

    [AM] Links produce JSON as agreed with Delius

---
+++ app/assets/javascripts/nationalsearch/containers/legacySearchLinkContainer.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux'
+import { legacySearch } from '../actions/navigate'
+import legacySearchLink from '../components/legacySearchLink.jsx'
+
+const mapStateToProps = () => {
+    return {}
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        legacySearch: () => {
+            dispatch(legacySearch())
+        }
+    }
+};
+
+const legacySearchLinkContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(legacySearchLink);
+
+export default legacySearchLinkContainer
--- test/assets/javascripts/nationalsearch/components/frameNavigationTest.js
+++ test/assets/javascripts/nationalsearch/components/frameNavigationTest.js
@@ -9,23 +9,32 @@ describe('FrameNavigation component', () => {
             global.parent.postMessage = (param) => message = param
             const navigationData = {
                 shouldClose: true,
-                nextPage: 'SomePage',
-                parameters: {
-                    param1: 'value1',
-                    param2: 'value2'
-                }
+                action: 'SomePage',
+                data: 'value'
             }
             const frameNavigation = shallow(<FrameNavigation navigate={navigationData}/>)
 
 
             expect(JSON.parse(message)).to.eql({
-                navigate: {
-                    nextPage: 'SomePage',
-                    parameters: {
-                        param1: 'value1',
-                        param2: 'value2'
-                    }
-                }
+                action: 'SomePage',
+                data: 'value'
+            })
+        })
+    })
+    context('a close navigation event with no data', () => {
+        it('postmessage on parent frame called', () => {
+            let message;
+            global.parent.postMessage = (param) => message = param
+            const navigationData = {
+                shouldClose: true,
+                action: 'SomePage'
+            }
+            const frameNavigation = shallow(<FrameNavigation navigate={navigationData}/>)
+
+
+            expect(JSON.parse(message)).to.eql({
+                action: 'SomePage',
+                data: null
             })
         })
     })
