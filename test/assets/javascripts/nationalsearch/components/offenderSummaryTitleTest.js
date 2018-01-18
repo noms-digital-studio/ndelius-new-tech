bd59a4f46ec23d5141a1413b9a3f17a91a13327b
Andy Marke <andy.marke@gmail.com>
2018-01-18 09:37:12 +0000

    [AM] Move to new aggreed offender data structure

--- test/assets/javascripts/nationalsearch/components/offenderSummaryTitleTest.js
+++ test/assets/javascripts/nationalsearch/components/offenderSummaryTitleTest.js
@@ -7,7 +7,7 @@ describe('OffenderSummaryTitle component', () => {
     context('link clicked', () => {
         it('showOffenderDetails callback function called with offenderId', () => {
             const showOffenderDetails = stub()
-            const title = shallow(<OffenderSummaryTitle offenderId={'123'}
+            const title = shallow(<OffenderSummaryTitle offenderId={123}
                                                         showOffenderDetails={showOffenderDetails}
                                                         firstName={'name'}
                                                         surname={'name'}
@@ -15,7 +15,7 @@ describe('OffenderSummaryTitle component', () => {
 
             title.find('a').simulate('click');
 
-            expect(showOffenderDetails).to.be.calledWith('123');
+            expect(showOffenderDetails).to.be.calledWith(123);
         })
     })
 })
