0ca7b49927ca9bcda80b269f075a9024fb324a37
Nick Redshaw <nickredshaw@gmail.com>
2018-01-31 16:59:58 +0000

    [NR] #172 - wip

--- app/assets/javascripts/nationalsearch/components/searchFooter.js
+++ app/assets/javascripts/nationalsearch/components/searchFooter.js
@@ -2,18 +2,18 @@ import LegacySearchLink from "./legacySearchLink";
 
 export default () => (
     <div>
-        <h3 className="heading-medium">Use this <strong className="phase-tag">ALPHA</strong> service to search for:</h3>
+        <h3 className="heading-medium">Use this <strong className="phase-tag">ALPHA</strong> service to search for offender's:</h3>
 
         <ul className="list-bullet margin-top medium">
-            <li>Offender names and dates of birth</li>
-            <li>Offender addresses and previous addresses</li>
-            <li>Identification numbers such as CRN, PNC, and National insurance</li>
-            <li>Offender aliases and dates of birth</li>
+            <li>names and dates of birth</li>
+            <li>addresses and previous addresses</li>
+            <li>identification numbers such as CRN, PNC, and National insurance</li>
+            <li>aliases and dates of birth</li>
         </ul>
 
         <div class="margin-top medium">&nbsp;</div>
 
         <div>
-            <span>You can access the <LegacySearchLink>previous version</LegacySearchLink> of the search here.</span>
+            <span>You can access the <LegacySearchLink>previous</LegacySearchLink> search here.</span>
         </div>
     </div>);
