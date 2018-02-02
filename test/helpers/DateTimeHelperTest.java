91b3c212217d0891a689d6e19b701685a2283d60
Nick Redshaw <nickredshaw@gmail.com>
2018-02-02 16:30:20 +0000

    [NR] #193 - Date helper for recognising date like strings

--- test/helpers/DateTimeHelperTest.java
+++ test/helpers/DateTimeHelperTest.java
@@ -18,4 +18,47 @@ public class DateTimeHelperTest {
         assertThat(DateTimeHelper.calculateAge("1954-02-01", fixedClock)).isEqualTo(15);
     }
 
+    @Test
+    public void returnsTrueForStringsThatAreValidDates() {
+        assertThat(DateTimeHelper.looksLikeADate("1992-02-01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-2-01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-2-1")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01-02-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1-12-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1-2-1992")).isEqualTo(true);
+
+        assertThat(DateTimeHelper.looksLikeADate("01-Feb-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01-feb-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1-Feb-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1-feb-1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-Feb-01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-feb-01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-Feb-1")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992-feb-1")).isEqualTo(true);
+
+        assertThat(DateTimeHelper.looksLikeADate("1992/02/01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992/2/01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992/2/1")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01/02/1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01/2/1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1/2/1992")).isEqualTo(true);
+
+        assertThat(DateTimeHelper.looksLikeADate("1992/Feb/01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992/feb/01")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992/Feb/1")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1992/feb/1")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01/Feb/1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("01/feb/1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1/Feb/1992")).isEqualTo(true);
+        assertThat(DateTimeHelper.looksLikeADate("1/feb/1992")).isEqualTo(true);
+
+    }
+
+    @Test
+    public void returnsFalseForStringsThatAreNotValidDates() {
+        assertThat(DateTimeHelper.looksLikeADate("1992 02 01")).isEqualTo(false);
+        assertThat(DateTimeHelper.looksLikeADate("1992:02:01")).isEqualTo(false);
+        assertThat(DateTimeHelper.looksLikeADate("1992:Feb:01")).isEqualTo(false);
+        assertThat(DateTimeHelper.looksLikeADate("foo bar")).isEqualTo(false);
+    }
 }
