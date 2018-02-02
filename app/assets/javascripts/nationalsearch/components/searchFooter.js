394c79312d9187e756408011fc50d5d2f010e038
Nick Redshaw <nickredshaw@gmail.com>
2018-02-02 11:50:03 +0000

    [NR] #172 - Copy changes

--- app/assets/javascripts/nationalsearch/components/searchFooter.js
+++ app/assets/javascripts/nationalsearch/components/searchFooter.js
@@ -2,19 +2,19 @@ import LegacySearchLink from "../containers/legacySearchLinkContainer";
 
 const SearchFooter = () => (
     <div>
-        <h3 className="heading-medium">Use this <strong className="phase-tag">ALPHA</strong> service to search for offender's:</h3>
+        <h3 className="heading-medium">Use this new service to search for an offender’s:</h3>
 
         <ul className="list-bullet margin-top medium">
-            <li>names and dates of birth</li>
-            <li>addresses and previous addresses</li>
-            <li>identification numbers such as CRN, PNC, and National insurance</li>
-            <li>aliases and dates of birth</li>
+            <li>name and date of birth</li>
+            <li>address and previous addresses</li>
+            <li>identification numbers such as CRN, PCN and National Insurance</li>
+            <li>aliases and other recorded dates of birth</li>
         </ul>
 
         <div className="margin-top medium">&nbsp;</div>
 
         <div>
-            <span>You can access the <LegacySearchLink>previous</LegacySearchLink> search here.</span>
+            <span>You can still access the <LegacySearchLink>previous version of this search.</LegacySearchLink> </span>
         </div>
     </div>);
 
