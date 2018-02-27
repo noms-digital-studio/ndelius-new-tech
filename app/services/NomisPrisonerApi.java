ffb3559a62516d4937f97d11430513bc0fda1ed6
Andy Marke <andy.marke@gmail.com>
2018-02-27 16:45:23 +0000

    [AM] 228 Return stock blank image when unable to retrieve NOMIS image

--- app/services/NomisPrisonerApi.java
+++ app/services/NomisPrisonerApi.java
@@ -1,25 +1,19 @@
 package services;
 
-import akka.stream.impl.io.ByteStringParser;
 import com.google.common.base.Strings;
 import com.typesafe.config.Config;
 import helpers.JsonHelper;
-import helpers.StaticImage;
 import interfaces.PrisonerApi;
 import interfaces.PrisonerApiToken;
-import org.apache.commons.compress.utils.IOUtils;
-import play.Environment;
 import play.Logger;
 import play.libs.ws.WSClient;
 import play.libs.ws.WSResponse;
 
 import javax.inject.Inject;
-import java.io.IOException;
 import java.util.Base64;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Function;
 
-import static helpers.StaticImage.noPhotoImage;
 import static play.mvc.Http.HeaderNames.AUTHORIZATION;
 import static play.mvc.Http.Status.OK;
 
@@ -28,16 +22,14 @@ public class NomisPrisonerApi implements PrisonerApi {
     private final String apiBaseUrl;
     private final WSClient wsClient;
     private final PrisonerApiToken apiToken;
-    private final Environment environment;
 
     @Inject
-    public NomisPrisonerApi(Config configuration, WSClient wsClient, PrisonerApiToken apiToken, Environment environment) {
+    public NomisPrisonerApi(Config configuration, WSClient wsClient, PrisonerApiToken apiToken) {
 
         apiBaseUrl = configuration.getString("nomis.api.url");
 
         this.wsClient = wsClient;
         this.apiToken = apiToken;
-        this.environment = environment;
     }
 
     @Override
@@ -65,7 +57,7 @@ public class NomisPrisonerApi implements PrisonerApi {
                     if (Strings.isNullOrEmpty(base64)) {
 
                         Logger.warn("Empty Image Base64 text for Nomis Id: {}", nomisId);
-                        return noPhotoImage(environment);
+                        throw new RuntimeException("No NOMIS image retrieved");
                     }
                     else {
                         return  Base64.getDecoder().decode(base64);
