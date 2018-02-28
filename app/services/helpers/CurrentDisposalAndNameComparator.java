b09346a3dbdd5c6b2996acd6575fcfaf484b0185
Nick Redshaw <nickredshaw@gmail.com>
2018-02-28 14:22:03 +0000

    [NR] #219 - group offenders with same names

--- app/services/helpers/CurrentDisposalAndNameComparator.java
+++ app/services/helpers/CurrentDisposalAndNameComparator.java
@@ -12,12 +12,12 @@ public class CurrentDisposalAndNameComparator {
     public static Comparator<SearchHit> currentDisposalAndNameComparator = (o1, o2) -> {
         JsonNode offender1 = parse(o1.getSourceAsString());
         JsonNode offender2 = parse(o2.getSourceAsString());
-        String s1 = safeGet("currentDisposal", offender1) +
-            safeGet("surname", offender1) +
-            safeGet("firstName", offender1);
-        String s2 = safeGet("currentDisposal", offender2) +
-            safeGet("surname", offender2) +
-            safeGet("firstName", offender2);
+        String s1 =
+            safeGet("currentDisposal", offender1) +
+            safeGet("surname", offender1);
+        String s2 =
+            safeGet("currentDisposal", offender2) +
+            safeGet("surname", offender2);
 
         return s2.compareTo(s1);
     };
