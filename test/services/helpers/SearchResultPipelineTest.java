d35b077615eda2f3afa72ae9e972507cf1f58adb
Nick Talbot <solutions@magichat.biz>
2018-03-01 09:19:49 +0000

    [NT]: Factored, Pipelined, simplified with closures, removed internal JsonNodes

--- test/services/helpers/SearchResultPipelineTest.java
+++ test/services/helpers/SearchResultPipelineTest.java
@@ -1,46 +1,55 @@
 package services.helpers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import lombok.val;
 import org.junit.Before;
 import org.junit.Test;
 import play.libs.Json;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SearchResultAppendersTest {
+public class SearchResultPipelineTest {
 
     private ObjectMapper mapper;
     private ObjectNode rootNode;
+    private Map<String, Map.Entry<Function<ObjectNode, Optional<JsonNode>>, BiFunction<ObjectNode, JsonNode, ObjectNode>>> pipeline;
 
     @Before
     public void setup() {
         mapper = Json.mapper();
         rootNode = mapper.createObjectNode();
+        pipeline = SearchResultPipeline.create(Function.identity(), "", "", ImmutableMap.of());
     }
 
     @Test
     public void appendsAgeIfDateOfBirthPresent() {
+
         rootNode.put("dateOfBirth", "2018-02-28");
-        ObjectNode newNode = SearchResultAppenders.appendOffendersAge(rootNode);
+        ObjectNode newNode = SearchResultPipeline.process(rootNode, ImmutableList.of(pipeline.get("addAgeAndHighlightDob")));
         assertThat(newNode.get("age")).isNotNull();
     }
 
     @Test
     public void doesNotAppendAgeIfDateOfBirthNotPresent() {
-        ObjectNode newNode = SearchResultAppenders.appendOffendersAge(rootNode);
+        ObjectNode newNode = SearchResultPipeline.process(rootNode, ImmutableList.of(pipeline.get("addAgeAndHighlightDob")));
         assertThat(newNode.get("age")).isNull();
     }
 
     @Test
     public void appendsOneTimeNomisRefIfNomsNumberPresent() {
-        Function<String, String> encrypter = string -> string;
         ObjectNode nomsNode = mapper.createObjectNode();
         nomsNode.put("nomsNumber", "A123");
         rootNode.set("otherIds", nomsNode);
-        ObjectNode newNode = SearchResultAppenders.appendOneTimeNomisRef("token", rootNode, encrypter);
+        ObjectNode newNode = SearchResultPipeline.process(rootNode, ImmutableList.of(pipeline.get("addOneTimeNomisRef")));
         assertThat(newNode.get("oneTimeNomisRef")).isNotNull();
         System.out.println(newNode.get("oneTimeNomisRef"));
         assertThat(newNode.get("oneTimeNomisRef").asText()
@@ -49,11 +58,10 @@ public class SearchResultAppendersTest {
 
     @Test
     public void doesNotAppendsOneTimeNomisRefIfnomsNumberNotPresent() {
-        Function<String, String> encrypter = string -> string;
         ObjectNode nomsNode = mapper.createObjectNode();
         nomsNode.put("pncNumber", "2018/12345");
         rootNode.set("otherIds", nomsNode);
-        ObjectNode newNode = SearchResultAppenders.appendOneTimeNomisRef("token", rootNode, encrypter);
+        ObjectNode newNode = SearchResultPipeline.process(rootNode, ImmutableList.of(pipeline.get("addOneTimeNomisRef")));
         assertThat(newNode.get("oneTimeNomisRef")).isNull();
     }
 }
