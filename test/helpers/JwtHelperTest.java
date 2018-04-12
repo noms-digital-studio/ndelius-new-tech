e0f34cb25351815b83af48044a0ec99de2498764
Andy Marke <andy.marke@gmail.com>
2018-04-12 11:58:47 +0100

    [AM, MA] 331 Improved JWT helper in test

--- test/helpers/JwtHelperTest.java
+++ test/helpers/JwtHelperTest.java
@@ -8,8 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JwtHelperTest {
 
-    public static final String FAKE_USER_BEARKER_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjbj1mYWtlLnVzZXIsY249VXNlcnMsZGM9bW9qLGRjPWNvbSIsInVpZCI6ImZha2UudXNlciIsImV4cCI6MTUxNzYzMTkzOX0=.FsI0VbLbqLRUGo7GXDEr0hHLvDRJjMQWcuEJCCaevXY1KAyJ_05I8V6wE6UqH7gB1Nq2Y4tY7-GgZN824dEOqQ";;
-
     @Test
     public void principalIsExtractedFromJwtSubject() {
 
@@ -26,7 +24,14 @@ public class JwtHelperTest {
                 .isEqualTo("cn=fake.user,cn=Users,dc=moj,dc=com");
     }
 
-    private String generateToken(String body) {
+    private static String generateToken(String body) {
         return String.format("eyJhbGciOiJIUzUxMiJ9.%s.FsI0VbLbqLRUGo7GXDEr0hHLvDRJjMQWcuEJCCaevXY1KAyJ_05I8V6wE6UqH7gB1Nq2Y4tY7-GgZN824dEOqQ", Base64.getEncoder().encodeToString(body.getBytes()));
+    }
+
+    public static String generateToken() {
+        return generateTokenWithSubject("cn=fake.user,cn=Users,dc=moj,dc=com");
+    }
+    public static String generateTokenWithSubject(String subject) {
+        return generateToken(String.format("{\"sub\":\"%s\"}", subject));
     }
 }
