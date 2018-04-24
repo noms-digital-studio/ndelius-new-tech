c92911047aa7119770f6ce47fff7de453c14eb8a
Paul Massey <paul.massey@scriptwerx.io>
2018-04-24 15:49:15 +0100

    Updated help block on search

--- app/assets/javascripts/nationalsearch/components/offenderSearchPage.js
+++ app/assets/javascripts/nationalsearch/components/offenderSearchPage.js
@@ -4,8 +4,8 @@ import FrameNavigation from '../containers/frameNavigationContainer';
 import AddNewOffenderLink from '../containers/addNewOffenderLinkContainer';
 import Suggestions from '../containers/suggestionsContainer';
 import GovUkPhaseBanner from './govukPhaseBanner';
-import SearchFooter from "./searchFooter";
-import PropTypes from "prop-types";
+import SearchFooter from './searchFooter';
+import PropTypes from 'prop-types';
 
 const OffenderSearchPage = ({firstTimeIn, showWelcomeBanner, reloadRecentSearch}) => {
 
@@ -18,15 +18,32 @@ const OffenderSearchPage = ({firstTimeIn, showWelcomeBanner, reloadRecentSearch}
             <div id="root">
                 <main id="content">
                     <GovUkPhaseBanner/>
-                    <div>
-                        <div className="govuk-box-highlight blue">
-                            <h1 className="heading-xlarge no-margin-top margin-bottom medium">Search for an offender</h1>
-                            <OffenderSearch/>
-                            <Suggestions/>
-                            <p className="font-medium bold margin-top medium no-margin-bottom">Can't find who you are looking
-                                for? <AddNewOffenderLink tabIndex="1"/></p></div>
+                    <div className="govuk-box-highlight blue">
+                        <div className="key-content search relative">
+
+                            <div className="padding-top padding-bottom search">
+                                <div className="grid-row">
+                                    <div className="column-two-thirds">
+                                        <h1 className="heading-xlarge margin-bottom medium no-margin-top">Search for an
+                                            offender</h1>
+                                    </div>
+                                    <div className="column-one-third align-right">
+                                        <AddNewOffenderLink tabIndex="1"/>
+                                    </div>
+                                </div>
+
+                                <OffenderSearch/>
+                                <Suggestions/>
+
+                            </div>
+
+                            <div className="national-search-help">
+                                <p className="no-margin">Not found what you are looking for?</p>
+                                <a className="font-medium bold clickable">Narrow down your search</a>
+                            </div>
+                        </div>
                     </div>
-                    <div className="padded mobile-pad key-content">
+                    <div className="key-content">
                         {showWelcomeBanner && <SearchFooter/>}
                         {!showWelcomeBanner && <OffenderSearchResults/>}
                     </div>
