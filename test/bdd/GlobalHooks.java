c4ff8784ffa6652c969cca9c4c310afe0c64c326
Andy Marke <andy.marke@gmail.com>
2018-07-11 09:08:46 +0100

    510 Refactor wiremocks

--- test/bdd/GlobalHooks.java
+++ test/bdd/GlobalHooks.java
@@ -1,10 +1,10 @@
 package bdd;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-import com.google.common.collect.ImmutableMap;
+import bdd.wiremock.AlfrescoStoreMock;
+import bdd.wiremock.OffenderApiMock;
+import bdd.wiremock.PdfGeneratorMock;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import helpers.JsonHelper;
 import interfaces.AnalyticsStore;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
@@ -12,63 +12,36 @@ import play.test.TestBrowser;
 import views.WithChromeBrowser;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.mockito.Mockito.mock;
 import static play.inject.Bindings.bind;
-import static play.libs.Json.toJson;
 
 public class GlobalHooks extends WithChromeBrowser {
     @Inject
     private TestBrowser theTestBrowser;
 
     @Inject
-    @Named("pdfWireMock")
-    private WireMockServer pdfWireMock;
+    private AlfrescoStoreMock alfrescoStoreMock;
     @Inject
-    @Named("alfrescofWireMock")
-    private WireMockServer alfrescofWireMock;
+    private PdfGeneratorMock pdfGeneratorMock;
     @Inject
-    @Named("offenderApiWireMock")
-    private WireMockServer offenderApiWireMock;
+    private OffenderApiMock offenderApiMock;
 
     @Before
     public void before() {
         startServer();
-        pdfWireMock.start();
-        alfrescofWireMock.start();
-        offenderApiWireMock.start();
-        pdfWireMock.stubFor(
-                post(urlEqualTo("/generate"))
-                        .willReturn(
-                                aResponse()
-                                        .withStatus(200)
-                                        .withBody(JsonHelper.stringify(new Byte[]{}))));
+        pdfGeneratorMock.start().stubDefaults();
+        offenderApiMock.start().stubDefaults();
+        alfrescoStoreMock.start().stubDefaults();
 
-        alfrescofWireMock.stubFor(
-                post(urlEqualTo("/noms-spg/uploadnew"))
-                        .willReturn(okForContentType("application/json", toJson(ImmutableMap.of("ID", "309db0bf-f8bb-4ac0-b325-5dbc368e2636")).toString())));
-
-        alfrescofWireMock.stubFor(
-                post(urlMatching("/noms-spg/uploadandrelease/.*"))
-                        .willReturn(okForContentType("application/json", toJson(ImmutableMap.of("ID", "309db0bf-f8bb-4ac0-b325-5dbc368e2636")).toString())));
-
-        alfrescofWireMock.stubFor(
-                post(urlMatching("/noms-spg/updatemetadata/.*"))
-                        .willReturn(okForContentType("application/json", toJson(ImmutableMap.of("ID", "309db0bf-f8bb-4ac0-b325-5dbc368e2636")).toString())));
-
-
-        offenderApiWireMock.stubFor(
-                post(urlEqualTo("/documentLink")).willReturn(created()));
         createBrowser();
     }
 
     @After
     public void after() {
-        pdfWireMock.stop();
-        alfrescofWireMock.stop();
-        offenderApiWireMock.stop();
+        pdfGeneratorMock.stop();
+        alfrescoStoreMock.stop();
+        offenderApiMock.stop();
         stopServer();
         quitBrowser();
     }
