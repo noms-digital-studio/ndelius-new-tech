898aa6102b80e08645e5bede13b9c5c36e648d3b
Paul Massey <paul.massey@scriptwerx.io>
2018-09-18 09:50:43 +0100

    Copy and paste mistake

--- test/bdd/paroleparom1report/RoshCustodySteps.java
+++ test/bdd/paroleparom1report/RoshCustodySteps.java
@@ -1,13 +1,13 @@
 package bdd.paroleparom1report;
 
 import cucumber.api.java.en.Given;
-import views.pages.paroleparom1report.RoshCommunityPage;
+import views.pages.paroleparom1report.RoshCustodyPage;
 
 import javax.inject.Inject;
 
 public class RoshCustodySteps {
     @Inject
-    private RoshCommunityPage page;
+    private RoshCustodyPage page;
 
     @Given("^Delius user is on the \"Current RoSH custody\" UI on the Parole Report$")
     public void thatTheDeliusUserIsOnThePageWithinTheParoleReport() {
