6bbdac3efb02352ba507c0b932fd7d0d1a1f45b9
Paul Massey <paul.massey@scriptwerx.io>
2019-01-15 11:58:34 +0000

    Included breach alert

--- app/assets/javascripts/offendersummary/containers/offenderAlertsContainer.js
+++ app/assets/javascripts/offendersummary/containers/offenderAlertsContainer.js
@@ -1,11 +1,10 @@
 import {connect} from 'react-redux'
-import offenderIdentity from '../components/offenderIdentity'
+import offenderAlerts from '../components/offenderAlerts'
 
 const mapStateToProps = state => ({
-    offenderDetails: state.offenderSummary.offenderDetails,
     offenderConvictions: state.offenderSummary.offenderConvictions
 })
 export default connect(
     mapStateToProps,
     null
-)(offenderIdentity)
+)(offenderAlerts)
