86d9ea8c586d2197d3fec1366b5834404fba7743
Paul Massey <paul.massey@scriptwerx.io>
2019-01-21 08:54:11 +0000

    Initial commit

--- app/assets/javascripts/offendersummary/components/offenderSummaryPage.js
+++ app/assets/javascripts/offendersummary/components/offenderSummaryPage.js
@@ -1,56 +1,70 @@
-import React, { Component, Fragment } from 'react';
-import * as PropTypes from 'prop-types';
-import GovUkPhaseBanner from './govukPhaseBanner';
-import ErrorMessage from './errorMessage';
-import OffenderIdentity from '../containers/offenderIdentityContainer';
-import OffenderDetails from '../containers/offenderDetailsContainer';
-import Registrations from '../containers/registrationsContainer';
-import SeriousRegistrations from '../containers/seriousRegistrationsContainer';
-import Convictions from '../containers/convictionsContainer';
-import Notes from '../containers/notesContainer';
-import OffenderManager from '../containers/offenderManagerContainer';
+import React, { Component, Fragment } from 'react'
+import * as PropTypes from 'prop-types'
+import GovUkPhaseBanner from './govukPhaseBanner'
+import ErrorMessage from './errorMessage'
+import OffenderIdentity from '../containers/offenderIdentityContainer'
+import OffenderDetails from '../containers/offenderDetailsContainer'
+import Registrations from '../containers/registrationsContainer'
+import SeriousRegistrations from '../containers/seriousRegistrationsContainer'
+import Convictions from '../containers/convictionsContainer'
+import Notes from '../containers/notesContainer'
+import OffenderManager from '../containers/offenderManagerContainer'
+
+import { nodeListForEach } from 'govuk-frontend/common'
+import Accordion from 'govuk-frontend/components/accordion/accordion'
 
 class OffenderSummaryPage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor (props) {
+    super(props)
+  }
 
-    componentWillMount() {
-        const { getOffenderDetails } = this.props;
-        getOffenderDetails();
-    }
+  componentWillMount () {
+    const {getOffenderDetails} = this.props
+    getOffenderDetails()
+  }
 
-    render() {
-        const { fetching, error } = this.props;
-        return (
-            <Fragment>
-                <GovUkPhaseBanner/>
-                { !fetching && !error &&
-                <div className="qa-main-content">
+  componentDidMount () {
+    setTimeout(() => {
+      const $accordions = document.querySelectorAll('[data-module="accordion"]')
+      nodeListForEach($accordions, function ($accordion) {
+        new Accordion($accordion).init()
+      })
+    }, 5000)
+  }
 
-                    <OffenderIdentity/>
-                    <SeriousRegistrations/>
-                    <Registrations/>
-                    <Convictions/>
-                    <OffenderManager/>
-                    <OffenderDetails/>
-                    <Notes/>
+  render () {
+    const {fetching, error} = this.props
+    return (
+      <Fragment>
+        <GovUkPhaseBanner/>
+        { !fetching && !error &&
+        <div className="qa-main-content">
 
-                </div>
-                }
-                { !fetching && error &&
-                <ErrorMessage
-                    message="Unfortunately, we cannot display you the offender's information at the moment. Please try again later."/>
-                }
-            </Fragment>
-        );
-    }
+          <OffenderIdentity/>
+          <SeriousRegistrations/>
+          <div className="govuk-accordion" data-module="accordion" id="accordion-offender-summary">
+            <Registrations/>
+            <Convictions/>
+            <OffenderManager/>
+            <OffenderDetails/>
+          </div>
+          <Notes/>
+
+        </div>
+        }
+        { !fetching && error &&
+        <ErrorMessage
+          message="Unfortunately, we cannot display you the offender's information at the moment. Please try again later."/>
+        }
+      </Fragment>
+    )
+  }
 }
 
 OffenderSummaryPage.propTypes = {
-    getOffenderDetails: PropTypes.func,
-    fetching: PropTypes.bool,
-    error: PropTypes.bool
-};
+  getOffenderDetails: PropTypes.func,
+  fetching: PropTypes.bool,
+  error: PropTypes.bool
+}
 
-export default OffenderSummaryPage;
+export default OffenderSummaryPage
