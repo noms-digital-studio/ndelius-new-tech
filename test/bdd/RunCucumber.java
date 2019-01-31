9659681b6932dfefc5bb039540ba23c5f09d4436
Paul Massey <paul.massey@scriptwerx.io>
2019-01-31 15:04:25 +0000

    Updated OPD Pathway UI and data

--- test/bdd/RunCucumber.java
+++ test/bdd/RunCucumber.java
@@ -6,6 +6,6 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"features/bdd"}, format = {"pretty"})
+@CucumberOptions(features={"features/bdd/paroleparom1report"}, format = {"pretty"})
 public class RunCucumber {
 }
